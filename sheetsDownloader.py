f26b21fd61cecb38d8d897a635be7ba43c489f62
Lukas Andriejunas <lukas.andriejunas@gmail.com>
2020-02-17 10:33:22 +0200

    Add files via upload

---
+++ sheetsDownloader.py
@@ -0,0 +1,32 @@
+import os
+import csv
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+
+# Sample Google Sheet document: https://docs.google.com/spreadsheets/d/1YoV-0EoBmDGLg96mbCCY5Sw56YpTC_G9GDkNw60TbuQ/
+# Don't forget to replace this ID with yours!
+docID = "1YoV-0EoBmDGLg96mbCCY5Sw56YpTC_G9GDkNw60TbuQ"
+
+client = gspread.authorize(credentials)
+spreadsheet = client.open_by_key(docID)
+
+csvFileNames = []
+for i, worksheet in enumerate(spreadsheet.worksheets()):
+    filename = str(i+1) + '_' + str(worksheet.title) + '.csv'
+    csvFileNames.append(filename)
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(worksheet.get_all_values())
+
+with pd.ExcelWriter(spreadsheet.title + '.xlsx') as writer:
+    for i in range(len(csvFileNames)):
+        read_file = pd.read_csv(csvFileNames[i])
+        newSheetName = csvFileNames[i].split('_', 1)[-1].replace('.csv', '')
+        read_file.to_excel(writer, sheet_name=newSheetName, index=None)
+
+for i in csvFileNames:
+    os.remove(i)
